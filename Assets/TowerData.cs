ddd36c39ce771dc77cce93b60b5751c3acf35a65
Eric <magid23@gmail.com>
2022-12-09 17:28:32 -0800

    Responding to comments from github and some minor refactoring.

--- Assets/TowerData.cs
+++ Assets/TowerData.cs
@@ -4,37 +4,49 @@ using System.Collections.Generic;
 public static class TowerData
 {
     public enum Stat {
-        damage,
-        range,
-        areaOfEffect,
-        attackSpeed,
-        armorPierce,
-        armorTear,
-        damageOverTime,
+        DAMAGE,
+        RANGE,
+        AREA_OF_EFFECT,
+        ATTACK_SPEED,
+        ARMOR_PIERCE,
+        ARMOR_TEAR,
+        DAMAGE_OVER_TIME,
     }
 
     public enum Mode {
-        multiplicative,
-        special,
+        MULTIPLICATIVE,
+        SPECIAL,
     }
 
-    // Each divergent ability (usually at upgrades 3 and 5) should have its own entry in this enum.
-    public enum AbilityEnum {
-        AcidStun,  // Armor Tear level 3.
+    public enum Tower {
+        SPITTING_ANT_TOWER,
     }
+
+    struct TowerInfo {
+        public Dictionary<Stat, float> startingStats;
+        public static Ability[][] abilities;
+    }
+
+    private static Dictionary<Tower, TowerInfo> towerInfoDic = new Dictionary<Tower, TowerInfo>();
 }
 
 public class Ability {
-    TowerData.AbilityEnum abilityEnum;
+
+    // Each divergent ability (usually at upgrades 3 and 5) should have its own entry in this enum.
+    public enum SpecialAbilityEnum {
+        ACID_STUN,  // Armor Tear upgrade tree level 3.
+    }
+
+    // This describes a single change to an attribute.
+    struct AttributeModifier {
+        public TowerData.Stat attribute;
+        public float mult;
+    }
+
+    SpecialAbilityEnum specialAbilityEnum;
     string name;
     string description;
-    TowerData.Stat[] abilityStat;  // What stat this ability affects, can be multiple stats.
     TowerData.Mode mode;
-    float[] mult;  // What multiplier should be applied to the stats, shuld be ordered the same as abilityStat;
     float cost;
-}
-
-public static class SpittingAnts {
-    private static Dictionary<TowerData.Stat, float> startingStats;
-    public static Ability[][] abilities;
+    AttributeModifier[] attributeModifiers;
 }
