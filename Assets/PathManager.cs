5c51caf98476473ac532ef0261b33de4461c2e8e
naphren <naphren@gmail.com>
2022-12-15 17:55:32 -0800

    Wiring up the waypoints with the path manager.

--- Assets/PathManager.cs
+++ Assets/PathManager.cs
@@ -1,14 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PathManager : MonoBehaviour {
-
+    
     void Start() {
-        
+        Waypoint[] waypoints = GetComponents<Waypoint>();
+        PopulateWaypoints(waypoints);
     }
 
     void Update() {
         
     }
+
+    void PopulateWaypoints(Waypoint[] waypoints) {
+        var coords = waypoints.ToList().Select(w => w.GetCoordinates());
+        int maxX = coords.Select(c => c.x).Max();
+        int maxY = coords.Select(c => c.y).Max();
+
+        Waypoint[,] grid = new Waypoint[maxX, maxY];
+        foreach (var waypoint in waypoints) {
+            Vector2Int coord = waypoint.GetCoordinates();
+            grid[coord.x, coord.y] = waypoint;
+        }
+
+        foreach (var waypoint in waypoints) {
+            Vector2Int coord = waypoint.GetCoordinates();
+            int rotations = waypoint.GetRotationSteps();
+            foreach (var exit in waypoint.exits) {
+                Waypoint.Direction dir = (Waypoint.Direction)(((int)exit + rotations) % 4);
+                Waypoint neighbor = new();
+                if (dir == Waypoint.Direction.UP && coord.y + 1 < maxY) {
+                    neighbor = grid[coord.x, coord.y + 1];
+                } else if (dir == Waypoint.Direction.RIGHT && coord.x + 1 < maxX) {
+                    neighbor = grid[coord.x + 1, coord.y];
+                } else if (dir == Waypoint.Direction.DOWN && coord.y - 1 >= 0) {
+                    neighbor = grid[coord.x, coord.y - 1];
+                } else if (dir == Waypoint.Direction.LEFT && coord.x - 1 >= 0) {
+                    neighbor = grid[coord.x + 1, coord.y];
+                } else {
+                    continue;
+                }
+
+                neighbor.prevWaypoints.Add(waypoint);
+                waypoint.nextWaypoints.Add(waypoint);
+
+            }
+        }
+    }
+
+
 }
--- Assets/Waypoint.cs
+++ Assets/Waypoint.cs
@@ -3,21 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Waypoint : MonoBehaviour {
+    public enum Direction {
+        UP = 0,
+        RIGHT = 1,
+        DOWN = 2,
+        LEFT = 3
+    }
+
     private readonly int tileSpacing = 10;
 
-    [SerializeField] List<Vector2Int> exits;
-    [SerializeField] List<Vector2Int> entrances;
-    void Start() {
+    [SerializeField] public List<Direction> exits = new();
+    //[SerializeField] List<Vector2Int> entrances;
+    private void Start() {
         
     }
 
-    void Update() {
+    private void Update() {
         
     }
+
+    public List<Waypoint> nextWaypoints = new();
+    public List<Waypoint> prevWaypoints = new();
 
     public Vector2Int GetCoordinates() {
         int x = Mathf.RoundToInt(transform.position.x / tileSpacing);
         int y = Mathf.RoundToInt(transform.position.y / tileSpacing);
         return new Vector2Int(x, y);
+    }
+
+    public int GetRotationSteps() {
+        return Mathf.RoundToInt(transform.rotation.eulerAngles.z / 90);
+    }
+
+    public Vector2Int DirectionToVec(Direction dir) {
+        return dir switch {
+            Direction.UP => new Vector2Int(0, 1),
+            Direction.RIGHT => new Vector2Int(1, 0),
+            Direction.DOWN => new Vector2Int(0, -1),
+            Direction.LEFT => new Vector2Int(-1, 0),
+            _ => new Vector2Int(0, 0),
+        };
     }
 }
