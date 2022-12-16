5b23ebce0373388462639700ed7ba66b69c670b0
Eric <magid23@gmail.com>
2022-12-15 17:32:01 -0800

    Renaming the SpecialAbilityEnum enums in accordance with the new standard.

--- Assets/TowerData.cs
+++ Assets/TowerData.cs
@@ -34,12 +34,12 @@ public class Ability {
 
     // Each divergent ability (usually at upgrades 3 and 5) should have its own entry in this enum.
     public enum SpecialAbilityEnum {
-        SA_ACID_STUN,  // Spitting Ant Armor Tear upgrade tree level 3.
-        SA_DOT_SLOW,  // Spitting Ant Acit DoT upgrade tree level 3.
-        SA_CAMO_SIGHT,  // Spitting Ant Utility upgrade tree level 3.
-        SA_TOTAL_TEAR_BONUS,  // Spitting Ant Armor Tear upgrade tree level 5.
-        SA_DOT_EXPLOSION,  // Spitting Ant Acid DoT upgrade tree level 5.
-        SA_CONSTANT_FIRE,  // Spitting Ant Utility upgrade tree level 5.
+        SA_1_3_ACID_STUN,  // Spitting Ant Armor Tear upgrade tree level 3.
+        SA_2_3_DOT_SLOW,  // Spitting Ant Acit DoT upgrade tree level 3.
+        SA_3_3_CAMO_SIGHT,  // Spitting Ant Utility upgrade tree level 3.
+        SA_1_5_TOTAL_TEAR_DAMAGE,  // Spitting Ant Armor Tear upgrade tree level 5.
+        SA_2_5_DOT_EXPLOSION,  // Spitting Ant Acid DoT upgrade tree level 5.
+        SA_3_5_CONSTANT_FIRE,  // Spitting Ant Utility upgrade tree level 5.
     }
 
     // This describes a single change to an attribute.
