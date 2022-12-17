c2495a4ecdec632f573134cb9c5e5922467e0fac
Eric <magid23@gmail.com>
2022-12-17 12:30:59 -0800

    Adding Unit Testing framework to the project and updating the gitignore to avoid bloat.

---
+++ InsectDefenseTest/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace InsectDefenseTest {
+    [TestClass]
+    public class InsectTestTest {
+        [TestMethod]
+        public void TestMethod11() {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
