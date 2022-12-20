a7f0a423d035d8704fcda39126bdeebb3ac2c908
Eric <magid23@gmail.com>
2022-12-19 17:14:41 -0800

    Correcting testing infrastructure for the project.

--- InsectDefenseTest/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace InsectDefenseTest {
-    [TestClass]
-    public class InsectTestTest {
-        [TestMethod]
-        public void TestMethod11() {
-            Assert.AreEqual(1, 1);
-        }
-    }
-}
