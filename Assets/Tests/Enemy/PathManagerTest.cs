289de58c6e07a69024ba4c5db8e90558a6ca6375
naphren <naphren@gmail.com>
2023-01-16 14:10:24 -0800

    Switching to 2 space tabs.

--- Assets/Tests/Enemy/PathManagerTest.cs
+++ Assets/Tests/Enemy/PathManagerTest.cs
@@ -7,87 +7,87 @@ using UnityEngine;
 
 
 public class PathManagerTest {
-    readonly int tileSpacing = 10;
+  readonly int tileSpacing = 10;
 
-    // Creates a plus orientation of five waypoints where the central waypoint has exits in all directions.
-    // Tests that all directions are handled correctly.
-    [Test]
-    public void PopulateWaypointsWorks() {
-        Waypoint waypointUp = GetWaypoint(Vector3.up * tileSpacing);
-        Waypoint waypointRight = GetWaypoint(Vector3.right * tileSpacing);
-        Waypoint waypointDown = GetWaypoint(Vector3.down * tileSpacing);
-        Waypoint waypointLeft = GetWaypoint(Vector3.left * tileSpacing);
-        Waypoint waypointCenter = GetWaypoint(Vector3.zero * tileSpacing);
+  // Creates a plus orientation of five waypoints where the central waypoint has exits in all directions.
+  // Tests that all directions are handled correctly.
+  [Test]
+  public void PopulateWaypointsWorks() {
+    Waypoint waypointUp = GetWaypoint(Vector3.up * tileSpacing);
+    Waypoint waypointRight = GetWaypoint(Vector3.right * tileSpacing);
+    Waypoint waypointDown = GetWaypoint(Vector3.down * tileSpacing);
+    Waypoint waypointLeft = GetWaypoint(Vector3.left * tileSpacing);
+    Waypoint waypointCenter = GetWaypoint(Vector3.zero * tileSpacing);
 
-        waypointCenter.exits =
-            Enum.GetValues(typeof(Waypoint.Direction)).OfType<Waypoint.Direction>().ToList();
-        
-        PathManager pathManager = new GameObject().AddComponent<PathManager>();
-        object[] args = { new Waypoint[] { waypointUp, waypointRight, waypointDown, waypointLeft, waypointCenter } };
-        InvokePopulateWaypoints(pathManager, args);
+    waypointCenter.exits =
+        Enum.GetValues(typeof(Waypoint.Direction)).OfType<Waypoint.Direction>().ToList();
 
-        Assert.That(waypointUp.prevWaypoints,
-            Is.EquivalentTo(new List<Waypoint>() { waypointCenter }));
-        Assert.That(waypointRight.prevWaypoints,
-            Is.EquivalentTo(new List<Waypoint>() { waypointCenter }));
-        Assert.That(waypointDown.prevWaypoints,
-            Is.EquivalentTo(new List<Waypoint>() { waypointCenter }));
-        Assert.That(waypointLeft.prevWaypoints,
-            Is.EquivalentTo(new List<Waypoint>() { waypointCenter }));
-        Assert.That(waypointCenter.nextWaypoints,
-            Is.EquivalentTo(new List<Waypoint>() { waypointUp, waypointRight, waypointDown, waypointLeft }));
-    }
+    PathManager pathManager = new GameObject().AddComponent<PathManager>();
+    object[] args = { new Waypoint[] { waypointUp, waypointRight, waypointDown, waypointLeft, waypointCenter } };
+    InvokePopulateWaypoints(pathManager, args);
 
-    // Creates a Waypoint with exits in all directions but no neighbors.
-    // Tests the bounds checking of the PathManager.
-    [Test]
-    public void PopulateWaypointsPointsOutOfBounds() {
-        Waypoint waypointCenter = GetWaypoint(Vector3.zero * tileSpacing);
+    Assert.That(waypointUp.prevWaypoints,
+        Is.EquivalentTo(new List<Waypoint>() { waypointCenter }));
+    Assert.That(waypointRight.prevWaypoints,
+        Is.EquivalentTo(new List<Waypoint>() { waypointCenter }));
+    Assert.That(waypointDown.prevWaypoints,
+        Is.EquivalentTo(new List<Waypoint>() { waypointCenter }));
+    Assert.That(waypointLeft.prevWaypoints,
+        Is.EquivalentTo(new List<Waypoint>() { waypointCenter }));
+    Assert.That(waypointCenter.nextWaypoints,
+        Is.EquivalentTo(new List<Waypoint>() { waypointUp, waypointRight, waypointDown, waypointLeft }));
+  }
 
-        waypointCenter.exits =
-            Enum.GetValues(typeof(Waypoint.Direction)).OfType<Waypoint.Direction>().ToList();
+  // Creates a Waypoint with exits in all directions but no neighbors.
+  // Tests the bounds checking of the PathManager.
+  [Test]
+  public void PopulateWaypointsPointsOutOfBounds() {
+    Waypoint waypointCenter = GetWaypoint(Vector3.zero * tileSpacing);
 
-        PathManager pathManager = new GameObject().AddComponent<PathManager>();
-        object[] args = { new Waypoint[] { waypointCenter } };
-        InvokePopulateWaypoints(pathManager, args);
+    waypointCenter.exits =
+        Enum.GetValues(typeof(Waypoint.Direction)).OfType<Waypoint.Direction>().ToList();
 
-        Assert.That(waypointCenter.nextWaypoints, Is.EquivalentTo(new List<Waypoint>()));
-    }
+    PathManager pathManager = new GameObject().AddComponent<PathManager>();
+    object[] args = { new Waypoint[] { waypointCenter } };
+    InvokePopulateWaypoints(pathManager, args);
 
-    // Creates a central Waypoint with neighbors above and to the right.  The central Waypoint is rotated -270 degrees.
-    // Tests that rotation of Waypoints is handled correctly.
-    [Test]
-    public void PopulateWaypointsRotation() {
-        Waypoint waypointUp = GetWaypoint(Vector3.up * tileSpacing);
-        Waypoint waypointRight = GetWaypoint(Vector3.right * tileSpacing);
-        Waypoint waypointCenter = GetWaypoint(Vector3.zero * tileSpacing);
+    Assert.That(waypointCenter.nextWaypoints, Is.EquivalentTo(new List<Waypoint>()));
+  }
 
-        waypointCenter.exits = new List<Waypoint.Direction>() { Waypoint.Direction.UP };
-        waypointCenter.transform.Rotate(-270*Vector3.forward);
+  // Creates a central Waypoint with neighbors above and to the right.  The central Waypoint is rotated -270 degrees.
+  // Tests that rotation of Waypoints is handled correctly.
+  [Test]
+  public void PopulateWaypointsRotation() {
+    Waypoint waypointUp = GetWaypoint(Vector3.up * tileSpacing);
+    Waypoint waypointRight = GetWaypoint(Vector3.right * tileSpacing);
+    Waypoint waypointCenter = GetWaypoint(Vector3.zero * tileSpacing);
 
-        PathManager pathManager = new GameObject().AddComponent<PathManager>();
-        object[] args = { new Waypoint[] { waypointUp, waypointRight, waypointCenter } };
-        InvokePopulateWaypoints(pathManager, args);
+    waypointCenter.exits = new List<Waypoint.Direction>() { Waypoint.Direction.UP };
+    waypointCenter.transform.Rotate(-270 * Vector3.forward);
 
-        Assert.That(waypointCenter.nextWaypoints, Is.EquivalentTo(new List<Waypoint>() { waypointRight }));
-        Assert.That(waypointUp.prevWaypoints, Is.EquivalentTo(new List<Waypoint>()));
-        Assert.That(waypointRight.prevWaypoints, Is.EquivalentTo(new List<Waypoint>() { waypointCenter }));
-    }
+    PathManager pathManager = new GameObject().AddComponent<PathManager>();
+    object[] args = { new Waypoint[] { waypointUp, waypointRight, waypointCenter } };
+    InvokePopulateWaypoints(pathManager, args);
 
-    // Creates and returns a Waypoint at location v.
-    Waypoint GetWaypoint(Vector3 v) {
-        GameObject gameObject = new();
-        gameObject.transform.position = v;
-        return gameObject.AddComponent<Waypoint>();
-    }
+    Assert.That(waypointCenter.nextWaypoints, Is.EquivalentTo(new List<Waypoint>() { waypointRight }));
+    Assert.That(waypointUp.prevWaypoints, Is.EquivalentTo(new List<Waypoint>()));
+    Assert.That(waypointRight.prevWaypoints, Is.EquivalentTo(new List<Waypoint>() { waypointCenter }));
+  }
 
-    // Uses reflection to call the private method PopulateWaypoints.
-    void InvokePopulateWaypoints(PathManager pathManager, object[] args) {
-        Type[] argTypes = { typeof(Waypoint[]) };
-        MethodInfo populateWaypoints = typeof(PathManager).GetMethod(
-            "PopulateWaypoints",
-            BindingFlags.NonPublic | BindingFlags.Instance,
-            null, CallingConventions.Standard, argTypes, null);
-        populateWaypoints.Invoke(pathManager, args);
-    }
+  // Creates and returns a Waypoint at location v.
+  Waypoint GetWaypoint(Vector3 v) {
+    GameObject gameObject = new();
+    gameObject.transform.position = v;
+    return gameObject.AddComponent<Waypoint>();
+  }
+
+  // Uses reflection to call the private method PopulateWaypoints.
+  void InvokePopulateWaypoints(PathManager pathManager, object[] args) {
+    Type[] argTypes = { typeof(Waypoint[]) };
+    MethodInfo populateWaypoints = typeof(PathManager).GetMethod(
+        "PopulateWaypoints",
+        BindingFlags.NonPublic | BindingFlags.Instance,
+        null, CallingConventions.Standard, argTypes, null);
+    populateWaypoints.Invoke(pathManager, args);
+  }
 }
