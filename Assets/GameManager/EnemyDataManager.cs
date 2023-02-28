378cd4236e7bfe72365edc472c0351abc125237c
naphren <naphren@gmail.com>
2023-02-27 16:44:04 -0800

    Adding some comments to EnemyDataManager and addressing a few comments.

--- Assets/GameManager/EnemyDataManager.cs
+++ Assets/GameManager/EnemyDataManager.cs
@@ -1,15 +1,16 @@
-using System.Collections;
+// A class that contains a dictionary of EnemyDatas keyed by strings.
+// Allows you to reference enemy stats by name for convenient wave creation.
 using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
 using UnityEngine;
 
-//using KvPair = System.Collections.Generic.KeyValuePair<string, EnemyData>;
-
 public class EnemyDataManager : MonoBehaviour {
   [SerializeField] private string filename;
   readonly private Dictionary<string, EnemyData> datas = new();
 
+  // A simple Key Value pair class since the builtin KeyValuePair does not
+  // serialize well.
   public class KvPair {
     public string Key;
     public EnemyData Value;
@@ -19,30 +20,39 @@ public class EnemyDataManager : MonoBehaviour {
     DeserializeEnemies(filename);
   }
 
+  // Returns the EnemyData associated with the key.
+  // Errors out if key is not contained in the dictionary.
   public EnemyData GetEnemyData(string key) {
     return datas[key];
   }
 
+  // A version of serialization that takes an arbitrary TextWriter.
+  // Not intended for general use but may be helpfull for testing.
   public void SerializeEnemies(List<KvPair> enemies, TextWriter writer) {
     XmlSerializer serializer = new(typeof(List<KvPair>));
     serializer.Serialize(writer, enemies);
   }
 
+  // Serializes the list of enemies to the file given by filename.
+  // Creates the file if it does not exist, otherwise overwrites it.
   public void SerializeEnemies(List<KvPair> enemies, string filename) {
     using TextWriter writer = new StreamWriter(filename);
     SerializeEnemies(enemies, writer);
   }
 
+  // A version of deserialization that takes an arbitrary Stream.
+  // Not inteded for general use but may be helpfull for testing.
   void DeserializeEnemies(Stream stream) {
     XmlSerializer serializer = new(typeof(List<KvPair>));
     List<KvPair> tmp = (List<KvPair>)serializer.Deserialize(stream);
 
     foreach (KvPair pair in tmp) {
-      Debug.Log("Key: " + pair.Key + " Value: " + pair.Value);
       datas.Add(pair.Key, pair.Value);
     }
   }
 
+  // Deserializes the list of KvPairs in filename and adds them to the
+  // EnemyData dictionary.
   void DeserializeEnemies(string filename) {
     using Stream reader = new FileStream(filename, FileMode.Open);
     DeserializeEnemies(reader);
--- Assets/PlayModeTests/Spawner/SpawnerTest.cs
+++ Assets/PlayModeTests/Spawner/SpawnerTest.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using System.Reflection;
-using System.Xml.Serialization;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
