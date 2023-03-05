89b7f4a24c975b9e93794c4e613f9dc345391dcd
Eric <magid23@gmail.com>
2023-03-05 12:00:20 -0800

    Branching commit for the web shooting spider.

--- Assets/Tower/TowerData.cs
+++ Assets/Tower/TowerData.cs
@@ -50,12 +50,18 @@ public class Ability {
 
   // Each divergent ability (usually at upgrades 3 and 5) should have its own entry in this enum.
   public enum SpecialAbilityEnum {
-    SA_1_3_ARMOR_TEAR_STUN,      // Spitting Ant Armor Tear upgrade tree level 3.
-    SA_1_5_ARMOR_TEAR_EXPLOSION,  // Spitting Ant Armor Tear upgrade tree level 5.
-    SA_2_3_DOT_SLOW,       // Spitting Ant Acit DoT upgrade tree level 3.
-    SA_2_5_DOT_EXPLOSION,    // Spitting Ant Acid DoT upgrade tree level 5.
-    SA_3_3_ANTI_AIR,     // Spitting Ant Utility upgrade tree level 3.
-    SA_3_5_CONSTANT_FIRE,    // Spitting Ant Utility upgrade tree level 5.
+    SA_1_3_ARMOR_TEAR_STUN,  // Spitting Ant, Armor Tear upgrade tree level 3.
+    SA_1_5_ARMOR_TEAR_EXPLOSION,  // Spitting Ant, Armor Tear upgrade tree level 5.
+    SA_2_3_DOT_SLOW,  // Spitting Ant, Acit DoT upgrade tree level 3.
+    SA_2_5_DOT_EXPLOSION,  // Spitting Ant, Acid DoT upgrade tree level 5.
+    SA_3_3_ANTI_AIR,  // Spitting Ant, Utility upgrade tree level 3.
+    SA_3_5_CONSTANT_FIRE,  // Spitting Ant, Utility upgrade tree level 5.
+    WSS_1_3_SLOW_STUN,  // Web Shooting Spider, Improved Slow upgrade tree level 3.
+    WSS_1_5_PERMANENT_SLOW,  // Web Shooting Spider, Improved Slow upgrade tree level 5.
+    WSS_2_3_AREA_SLOW,  // Web Shooting Spider, AoE upgrade tree level 3.
+    WSS_2_5_LINGERING_SLOW,  // Web Shooting Spider, AoE upgrade tree level 5.
+    WSS_3_3_ANTI_AIR,  // Web Shooting Spider, Utility tree level 3.
+    WSS_3_5_AA_ASSISTANCE,  // Web Shooting Spider, Utility tree level 5.
   }
 
   // This describes a single change to an attribute.
