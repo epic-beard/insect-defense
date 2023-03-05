bbb854cee537e802cc6456dd237be38f86dfde5b
naphren <naphren@gmail.com>
2023-03-05 15:45:57 -0800

    alphetatizing some stuff.

--- Assets/Tower/TowerData.cs
+++ Assets/Tower/TowerData.cs
@@ -6,27 +6,27 @@ public struct TowerData {
     SPITTING_ANT_TOWER,
   }
   public enum Stat {
-    DAMAGE,
-    RANGE,
     AREA_OF_EFFECT,
-    ATTACK_SPEED,
     ARMOR_PIERCE,
     ARMOR_TEAR,
+    ATTACK_SPEED,
+    DAMAGE,
     DAMAGE_OVER_TIME,
     PROJECTILE_SPEED,
+    RANGE,
     SLOW_DURATION,
     SLOW_POWER,  // This is a percentage slow from 0.0 - 1.0.
     STUN_TIME,  // Slow duration in seconds.
   }
 
-  public float damage;
-  public float range;
   public float area_of_effect;
-  public float attack_speed;
   public float armor_pierce;
   public float armor_tear;
+  public float attack_speed;
+  public float damage;
   public float damage_over_time;
   public float projectile_speed;
+  public float range;
   public float slow_duration;
   public float slow_power;
   public float stun_time;
@@ -34,14 +34,14 @@ public struct TowerData {
   public float this[Stat stat] {
     get {
       return stat switch {
-        Stat.DAMAGE => damage,
-        Stat.RANGE => range,
         Stat.AREA_OF_EFFECT => area_of_effect,
-        Stat.ATTACK_SPEED => attack_speed,
         Stat.ARMOR_PIERCE => armor_pierce,
         Stat.ARMOR_TEAR => armor_tear,
+        Stat.ATTACK_SPEED => attack_speed,
+        Stat.DAMAGE => damage,
         Stat.DAMAGE_OVER_TIME => damage_over_time,
         Stat.PROJECTILE_SPEED => projectile_speed,
+        Stat.RANGE => range,
         Stat.SLOW_DURATION => slow_duration,
         Stat.SLOW_POWER => slow_power,
         Stat.STUN_TIME => stun_time,
@@ -50,14 +50,14 @@ public struct TowerData {
     }
     set {
       switch (stat) {
-        case Stat.DAMAGE: damage = value; break;
-        case Stat.RANGE: range = value; break;
         case Stat.AREA_OF_EFFECT: area_of_effect = value; break;
-        case Stat.ATTACK_SPEED: attack_speed = value; break;
         case Stat.ARMOR_PIERCE: armor_pierce = value; break;
         case Stat.ARMOR_TEAR: armor_tear = value; break;
+        case Stat.ATTACK_SPEED: attack_speed = value; break;
+        case Stat.DAMAGE: damage = value; break;
         case Stat.DAMAGE_OVER_TIME: damage_over_time = value; break;
         case Stat.PROJECTILE_SPEED: projectile_speed = value; break;
+        case Stat.RANGE: range = value; break;
         case Stat.SLOW_DURATION: slow_duration = value; break;
         case Stat.SLOW_POWER: slow_power = value; break;
         case Stat.STUN_TIME: stun_time = value; break;
