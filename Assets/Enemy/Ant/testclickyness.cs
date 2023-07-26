edd1fe158f53a4354b0702c8b0d78addd631709c
Eric <magid23@gmail.com>
2023-07-26 16:29:13 -0700

    Add a collider to the ant prefab.

---
+++ Assets/Enemy/Ant/testclickyness.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testclickyness : MonoBehaviour
+{
+  private void OnMouseDown() {
+    Debug.Log("This beastie has been clicked!");
+  }
+}
