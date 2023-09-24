df7870778af48ea3ff663f5c6c281197c853402a
naphren <naphren@gmail.com>
2023-09-24 16:43:23 -0700

    Small bug fixes and the start of the assassin bug tower.

---
+++ Assets/TowerDataGenerator/TowerDataGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDataGenerator {
+  class TowerDataGenerator {
+    static void Main(string[] args) {
+      Console.WriteLine("Poop");
+    }
+  }
+}
