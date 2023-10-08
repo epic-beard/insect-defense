47cb56d402b3079d847e205a3c09afe41d44afff
Eric <magid23@gmail.com>
2023-10-07 17:08:11 -0700

    Remove test class.

--- Assets/Tower/MantisTower/MantisCollisionSickle.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MantisCollisionSickle : MonoBehaviour {
-  private void OnTriggerEnter(Collider other) {
-    Debug.Log("Collision detected with the sickle specifically.");
-  }
-}
