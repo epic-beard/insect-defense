9fb22bf313378c69dfa7ff192f64136116ff1e9c
Eric <magid23@gmail.com>
2024-01-18 18:41:03 -0800

    Rebalance the waves for the first level and refactor the wave generator.

--- Assets/Tests/Generators/WaveGenerator.cs
+++ Assets/Tests/Generators/WaveGenerator.cs
@@ -14,72 +14,107 @@ public class WaveGenerator {
   }
 
   private void GenerateWave() {
-    DialogueBoxWave greeting = new() {
-      messages = { "Hello" }
-    };
-
-    CannedEnemyWave firstWaveAnt = new() {
-      enemyDataKey = ant,
-      repetitions = 6,
-      repeatDelay = 2.0f,
-      spawnLocation = 0,
-      spawnAmmount = 1,
-    };
-
     SequentialWave firstWave = new() {
-      Subwaves = { greeting, firstWaveAnt },
+      Subwaves = {
+        new DialogueBoxWave() {
+          messages =
+              { "Welcome to Insect Defense, where you kill bugs, with bugs.",
+                "Select a tower from the right hand menu and click on a space of grass to build it.",
+                "Towers get much more expensive very quickly, be careful!",
+                "For now, just build one Spitting Ant Tower." }
+        },
+        new CannedEnemyWave() {
+          enemyDataKey = ant,
+          repetitions = 6,
+          repeatDelay = 2.0f,
+          spawnLocation = 0,
+          spawnAmmount = 1,
+        },
+        new SpacerWave() {
+          delay = 2.0f,
+
+        },
+        new DialogueBoxWave() {
+          messages = { "Now might be a good time to build a second tower." }
+
+        },
+        new CannedEnemyWave() {
+          enemyDataKey = ant,
+          repetitions = 3,
+          repeatDelay = 1.0f,
+          spawnLocation = 0,
+          spawnAmmount = 2,
+        },
+      },
     };
 
-    CannedEnemyWave secondWaveAnt = new() {
-      enemyDataKey = ant,
-      repetitions = 3,
-      repeatDelay = 3.0f,
-      spawnLocation = 0,
-      spawnAmmount = 1,
-    };
-    CannedEnemyWave secondWaveAphid = new() {
-      enemyDataKey = aphid,
-      repetitions = 3,
-      repeatDelay = 3.0f,
-      spawnLocation = 0,
-      spawnAmmount = 3,
-    };
     ConcurrentWave secondWave = new() {
-      Subwaves = { secondWaveAnt, secondWaveAphid },
+      Subwaves = {
+        new DialogueBoxWave() {
+          messages = { "It looks like you have some Nu left, it is about time to upgrade a tower.",
+                       "First, click on the tower you wish to ugprade to select it.",
+                       "Now check out the available upgrades.",
+                       "I'll leave the next wave to you." }
+        },
+        new CannedEnemyWave() {
+          enemyDataKey = ant,
+          repetitions = 5,
+          repeatDelay = 3.0f,
+          spawnLocation = 0,
+          spawnAmmount = 2,
+        },
+        new CannedEnemyWave() {
+          enemyDataKey = aphid,
+          repetitions = 15,
+          repeatDelay = 1.0f,
+          spawnLocation = 0,
+          spawnAmmount = 1,
+        },
+      },
     };
 
-    CannedEnemyWave thirdWaveAnt = new() {
-      enemyDataKey = ant,
-      repetitions = 3,
-      repeatDelay = 2.0f,
-      spawnLocation = 0,
-      spawnAmmount = 1,
-    };
-    CannedEnemyWave thirdWaveAphid = new() {
-      enemyDataKey = aphid,
-      repetitions = 3,
-      repeatDelay = 2.0f,
-      spawnLocation = 0,
-      spawnAmmount = 3,
-    };
-    ConcurrentWave thirdWaveAntAphid = new() {
-      Subwaves = { thirdWaveAnt, thirdWaveAphid },
-    };
-    SpacerWave thirdWaveSpacer = new() {
-      delay = 1.0f,
-    };
-    DialogueBoxWave beetleWarning = new() {
-      messages = { "Beetles are armored enemies, make sure you have some armor tear or peirce to damage them!", "Check the Spitting Ant Tower upgrades." }
-    };
-    CannedEnemyWave thirdWaveBeetle = new() {
-      enemyDataKey = beetle,
-      repetitions = 1,
-      repeatDelay = 0.0f,
-      spawnLocation = 0,
-      spawnAmmount = 1,
-    };
     SequentialWave thirdWave = new() {
-      Subwaves = { thirdWaveAntAphid, thirdWaveSpacer, beetleWarning, thirdWaveBeetle },
+      Subwaves = {
+        new ConcurrentWave {
+          Subwaves = {
+            new CannedEnemyWave() {
+              enemyDataKey = ant,
+              repetitions = 3,
+              repeatDelay = 2.0f,
+              spawnLocation = 0,
+              spawnAmmount = 2,
+            },
+            new CannedEnemyWave() {
+              enemyDataKey = aphid,
+              repetitions = 3,
+              repeatDelay = 2.0f,
+              spawnLocation = 0,
+              spawnAmmount = 4,
+            },
+          },
+        },
+        new SpacerWave() {
+          delay = 1.0f,
+        },
+        new DialogueBoxWave() {
+          messages = {
+              "Beetles are armored enemies, make sure you have some armor tear or peirce to damage them!",
+              "Check the Spitting Ant Tower upgrades."
+          },
+        },
+        new CannedEnemyWave {
+          enemyDataKey = beetle,
+          repetitions = 2,
+          repeatDelay = 2.0f,
+          spawnLocation = 0,
+          spawnAmmount = 1,
+        },
+        new DialogueBoxWave {
+          messages = {
+              "Congratulations! You beat the first level. Time to return to the lab and plan your next steps."
+          },
+        },
+      },
     };
 
     Waves waves = new() {
