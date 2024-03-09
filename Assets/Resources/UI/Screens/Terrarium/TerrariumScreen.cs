04b1b066ab3507fcd647894e06562f1e857c5143
Amr Gaber <amrtgaber@protonmail.com>
2024-03-09 01:09:39 -0700

    clean up message box and terrarium screen
    Moves all styles to uss file, standardizes naming conventions, and refactors names for clarity.

--- Assets/Resources/UI/Screens/Terrarium/TerrariumScreen.cs
+++ Assets/Resources/UI/Screens/Terrarium/TerrariumScreen.cs
@@ -4,18 +4,18 @@ using UnityEngine.UIElements;
 public class TerrariumScreen : MonoBehaviour {
   public static TerrariumScreen Instance;
 
-  readonly private string tooltipVisualElementName = "tooltip__visualelement";
-  readonly private string tooltipLabelName = "tooltip__label";
-  readonly private string rightVEName = "right__ve";
-  readonly private string bottomVEName = "bottom__ve";
-  readonly private string messageBoxVEName = "i_message_box__ve";
+  readonly private string tooltipContainerVEName = "tooltip-container";
+  readonly private string tooltipLabelVEName = "tooltip-label";
+  readonly private string sidebarVEName = "sidebar";
+  readonly private string bottomBarVEName = "bottom-bar-parent";
+  readonly private string messageBoxVEName = "message-box-container";
 
   public VisualElement TooltipVE { get; private set; }
   public Label TooltipLabel { get; private set; }
 
-  private UIDocument terrariumScreen;
-  private VisualElement rightVE;
-  private VisualElement bottomVE;
+  private UIDocument uiDocument;
+  private VisualElement sidebarVE;
+  private VisualElement bottomBarVE;
   private VisualElement messageBoxVE;
 
   private void Awake() {
@@ -25,24 +25,24 @@ public class TerrariumScreen : MonoBehaviour {
   }
 
   void SetVisualElements() {
-    terrariumScreen = GetComponent<UIDocument>();
-    VisualElement rootElement = terrariumScreen.rootVisualElement;
+    uiDocument = GetComponent<UIDocument>();
+    VisualElement rootElement = uiDocument.rootVisualElement;
 
-    rightVE = rootElement.Q<VisualElement>(rightVEName);
-    bottomVE = rootElement.Q<VisualElement>(bottomVEName);
+    sidebarVE = rootElement.Q<VisualElement>(sidebarVEName);
+    bottomBarVE = rootElement.Q<VisualElement>(bottomBarVEName);
     messageBoxVE = rootElement.Q<VisualElement>(messageBoxVEName);
 
-    TooltipVE = rootElement.Q<VisualElement>(tooltipVisualElementName);
-    TooltipLabel = rootElement.Q<Label>(tooltipLabelName);
+    TooltipVE = rootElement.Q<VisualElement>(tooltipContainerVEName);
+    TooltipLabel = rootElement.Q<Label>(tooltipLabelVEName);
   }
 
   private void RegisterCallbacks() {
-    rightVE.RegisterCallback<MouseEnterEvent>(OnMouseOverUI);
-    bottomVE.RegisterCallback<MouseEnterEvent>(OnMouseOverUI);
+    sidebarVE.RegisterCallback<MouseEnterEvent>(OnMouseOverUI);
+    bottomBarVE.RegisterCallback<MouseEnterEvent>(OnMouseOverUI);
     messageBoxVE.RegisterCallback<MouseEnterEvent>(OnMouseOverUI);
 
-    rightVE.RegisterCallback<MouseLeaveEvent>(OnMouseLeaveUI);
-    bottomVE.RegisterCallback<MouseLeaveEvent>(OnMouseLeaveUI);
+    sidebarVE.RegisterCallback<MouseLeaveEvent>(OnMouseLeaveUI);
+    bottomBarVE.RegisterCallback<MouseLeaveEvent>(OnMouseLeaveUI);
     messageBoxVE.RegisterCallback<MouseLeaveEvent>(OnMouseLeaveUI);
   }
 
@@ -55,10 +55,10 @@ public class TerrariumScreen : MonoBehaviour {
   }
 
   public void HideUI() {
-    terrariumScreen.rootVisualElement.style.display = DisplayStyle.None;
+    uiDocument.rootVisualElement.style.display = DisplayStyle.None;
   }
 
   public void ShowUI() {
-    terrariumScreen.rootVisualElement.style.display = DisplayStyle.Flex;
+    uiDocument.rootVisualElement.style.display = DisplayStyle.Flex;
   }
 }
