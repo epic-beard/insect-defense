960a6e1707abaf041b507b63569a5ea5942fb75e
naphren <naphren@gmail.com>
2024-03-21 14:01:29 -0700

    Get the damage text facing the right direction.

--- Assets/Resources/UI/Screens/Terrarium/DamageText/DamageText.cs
+++ Assets/Resources/UI/Screens/Terrarium/DamageText/DamageText.cs
@@ -44,7 +44,11 @@ public class DamageText : MonoBehaviour {
   }
 
   private void FaceCamera() {
-    transform.LookAt(-Camera.main.transform.position, Vector3.up);
+    Vector3 newForward = Camera.main.transform.forward;
+    newForward.y = 0;
+    transform.forward = newForward;
+
+    //transform.LookAt(-Camera.main.transform.position, Vector3.up);
   }
 
   private IEnumerator BounceUp(Vector3 start) {
