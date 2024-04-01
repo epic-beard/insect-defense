a429ced624fbc880bcbffdc0b972d4a2f9112002
Eric <magid23@gmail.com>
2024-04-01 15:39:18 -0700

    Marked all imports with "Bake axis conversion" if not already marked and moved Enemy to the resources folder.

--- Assets/Spawner/DebugSpawner.cs
+++ Assets/Spawner/DebugSpawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,13 +7,26 @@ public class DebugSpawner : MonoBehaviour {
   [SerializeField] private EnemyData data;
 
   private void Start() {
+    Enum.GetValues(typeof(EnemyData.Type));
+    // TODO: Figure out why the hercules beetle and wolf spider cause crashes.
     var types = new HashSet<EnemyData.Type>() {
       EnemyData.Type.ANT,
       EnemyData.Type.APHID,
       EnemyData.Type.BEETLE,
+      //EnemyData.Type.HERCULES_BEETLE,
       EnemyData.Type.LEAF_BUG,
-      EnemyData.Type.TARANTULA
+      EnemyData.Type.SLUG,
+      EnemyData.Type.SNAIL,
+      EnemyData.Type.STINK_BUG,
+      EnemyData.Type.TARANTULA,
+      EnemyData.Type.TERMITE,
+      //EnemyData.Type.WOLF_SPIDER,
     };
+
+    // Use this code once the crash issue has been solved. Don't forget linq.
+    //HashSet<EnemyData.Type> allEnemyTypes =
+    //  new HashSet<EnemyData.Type>(Enum.GetValues(typeof(EnemyData.Type)).OfType<EnemyData.Type>().ToList());
+
     ObjectPool.Instance.InitializeObjectPool(types);
   }
 
